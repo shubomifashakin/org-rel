9c788c9f8bf355904a16ef10b1e4b2ad7de6a317
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-14 23:58:08 +0100

    implemented onModuleDestroy for databaseService

--- src/core/database/database.service.ts
+++ src/core/database/database.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../../generated/prisma/client.js';
 import env from '../serverEnv/index.js';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnModuleInit {
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super({
       transactionOptions: { maxWait: 10000, timeout: 10000 },
