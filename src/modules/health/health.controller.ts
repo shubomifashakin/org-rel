d317deeaef1087c9bca4431277ff891de1ca6d03
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-14 22:06:13 +0100

    removed throttling from health

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service.js';
+import { SkipThrottle } from '@nestjs/throttler';
 
 @Controller('health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
 
+  @SkipThrottle({ default: true })
   @Get()
   getHealth() {
     return this.healthService.getHealth();
