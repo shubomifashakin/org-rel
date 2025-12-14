4f72d7624a8d51970418ff1100a235531c951130
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-14 22:07:20 +0100

    enabled trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import env from './core/serverEnv/index.js';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: { methods: ['GET', 'POST', 'PUT', 'DELETE'], origin: '*' },
   });
 
   app.setGlobalPrefix('api/v1');
+  app.set('trust proxy', true);
   app.enableShutdownHooks(['SIGINT', 'SIGTERM']);
   await app.listen(env.PORT);
 }
