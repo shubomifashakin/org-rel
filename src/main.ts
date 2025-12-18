240dae6bf90b6bdabbce9f549e5440f23750edae
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-18 19:46:23 +0100

    setup cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module.js';
-import env from './core/serverEnv/index.js';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import compression from 'compression';
+import cookieParser from 'cookie-parser';
+
+import { AppModule } from './app.module.js';
+import env from './core/serverEnv/index.js';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -13,6 +15,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
   app.use(compression());
+  app.use(cookieParser());
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.set('trust proxy', true);
   app.enableShutdownHooks(['SIGINT', 'SIGTERM']);
