bfbbac3145cf8011b0dcf097030c119bed7fa621
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-18 19:51:35 +0100

    refactor

--- src/core/secrets-manager/secrets-manager.service.spec.ts
+++ src/core/secrets-manager/secrets-manager.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { SecretsManagerService } from './secrets-manager.service';
+import { SecretsManagerService } from './secrets-manager.service.js';
 
 describe('SecretsManagerService', () => {
   let service: SecretsManagerService;
