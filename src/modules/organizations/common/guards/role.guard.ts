106fdbc627940fc1560ddb79638433feb8911b45
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-21 17:02:27 +0100

    refactor

--- src/modules/organizations/common/guards/role.guard.ts
+++ src/modules/organizations/common/guards/role.guard.ts
@@ -33,7 +33,7 @@ export class RolesGuard implements CanActivate {
       context.getHandler(),
     );
 
-    if (!requiredRolesForHandler) {
+    if (!requiredRolesForHandler || !requiredRolesForHandler?.length) {
       return true;
     }
 
