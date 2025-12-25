e76560d174cd91ac31f1a376f6e8e945a37ce20f
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-25 13:12:27 +0100

    tests for hasher service

--- src/core/hasher/hasher.service.spec.ts
+++ src/core/hasher/hasher.service.spec.ts
@@ -3,6 +3,7 @@ import { HasherService } from './hasher.service.js';
 
 describe('HasherService', () => {
   let service: HasherService;
+  let hashedString: string;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,5 +15,33 @@ describe('HasherService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('hashString - it should hash a string', async () => {
+    const result = await service.hashString('test');
+    expect(result.status).toBe(true);
+    expect(result.error).toBe(null);
+    expect(result.data).toStrictEqual(expect.any(String));
+    hashedString = result.data!;
+  });
+
+  it('compareHashString - it should be valid', async () => {
+    const result = await service.compareHashedString({
+      plainString: 'test',
+      hash: hashedString,
+    });
+    expect(result.status).toBe(true);
+    expect(result.error).toBe(null);
+    expect(result.data).toBe(true);
+  });
+
+  it('compareHashString - it should not be valid', async () => {
+    const result = await service.compareHashedString({
+      plainString: 'testss',
+      hash: hashedString,
+    });
+    expect(result.status).toBe(true);
+    expect(result.error).toBe(null);
+    expect(result.data).toBe(false);
   });
 });
