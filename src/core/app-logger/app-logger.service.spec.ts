c1ea1261f8e394b5511bc989e99e091b1c069964
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-25 21:35:13 +0100

    updated tests

--- src/core/app-logger/app-logger.service.spec.ts
+++ src/core/app-logger/app-logger.service.spec.ts
@@ -1,5 +1,8 @@
+import { ClsModule } from 'nestjs-cls';
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppLoggerService } from './app-logger.service.js';
+import { AppConfigModule } from '../app-config/app-config.module.js';
 
 describe('AppLoggerService', () => {
   let service: AppLoggerService;
@@ -7,6 +10,21 @@ describe('AppLoggerService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AppLoggerService],
+      imports: [
+        AppConfigModule,
+        ClsModule.forRoot({
+          global: true,
+          middleware: {
+            mount: true,
+            generateId: true,
+            idGenerator: () => 'test-request-id',
+            setup: (clx) => {
+              clx.set('ip', '127.0.0.1');
+              clx.set('userAgent', 'test-agent');
+            },
+          },
+        }),
+      ],
     }).compile();
 
     service = module.get<AppLoggerService>(AppLoggerService);
