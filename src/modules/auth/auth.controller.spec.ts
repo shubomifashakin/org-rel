9ed8f710ad80efdf41861e95ca8e2f330f9baf0f
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-25 09:23:45 +0100

    testing

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -13,35 +13,44 @@ import { AppConfigModule } from '../../core/app-config/app-config.module.js';
 import { AppLoggerModule } from '../../core/app-logger/app-logger.module.js';
 import { DatabaseService } from '../../core/database/database.service.js';
 import { RedisService } from '../../core/redis/redis.service.js';
+import { ClsModule } from 'nestjs-cls';
+import { SecretsManagerService } from '../../core/secrets-manager/secrets-manager.service.js';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client.js';
 
 const mockResponse = {
-  cookie: jest.fn().mockReturnThis(),
-  status: jest.fn().mockReturnThis(),
-  json: jest.fn().mockReturnThis(),
+  cookie: jest.fn(),
+  status: jest.fn(),
+  json: jest.fn(),
 } as unknown as Response;
 
 const mockUser = {
   id: 'mock-user-id',
   username: 'testuser',
   email: 'test@example.com',
+  password: 'password',
 };
 
 const myDatabaseServiceMock = {
   users: {
-    create: jest.fn().mockResolvedValue(mockUser),
-    findUnique: jest.fn().mockResolvedValue(null),
+    create: jest.fn(),
+    findUnique: jest.fn(),
   },
   refreshTokens: {
-    create: jest.fn().mockResolvedValue(null),
+    create: jest.fn(),
   },
 };
 
-const mockRedisServiceMock = {
-  getFromCache: jest.fn().mockResolvedValue({
-    status: true,
-    data: { JWT_SECRET: '123456' },
-    error: null,
-  }),
+const myRedisServiceMock = {
+  getFromCache: jest.fn(),
+  setInCache: jest.fn(),
+};
+
+const mySecretsManagerServiceMock = {
+  getSecret: jest.fn(),
 };
 
 describe('AuthController', () => {
@@ -57,6 +66,18 @@ describe('AuthController', () => {
         S3Module,
         AppConfigModule,
         JwtServiceModule,
+        ClsModule.forRoot({
+          global: true,
+          middleware: {
+            mount: true,
+            generateId: true,
+            idGenerator: () => 'test-request-id',
+            setup: (clx) => {
+              clx.set('ip', '127.0.0.1');
+              clx.set('userAgent', 'test-agent');
+            },
+          },
+        }),
         AppLoggerModule,
       ],
       providers: [AuthService],
@@ -64,10 +85,14 @@ describe('AuthController', () => {
       .overrideProvider(DatabaseService)
       .useValue(myDatabaseServiceMock)
       .overrideProvider(RedisService)
-      .useValue(mockRedisServiceMock)
+      .useValue(myRedisServiceMock)
+      .overrideProvider(SecretsManagerService)
+      .useValue(mySecretsManagerServiceMock)
       .compile();
 
     controller = await module.resolve<AuthController>(AuthController);
+
+    jest.resetAllMocks();
   });
 
   it('should be defined', () => {
@@ -75,24 +100,123 @@ describe('AuthController', () => {
   });
 
   describe('Successful Requests', () => {
-    describe('Sign Up', () => {
-      it('signUp - it should create the user', async () => {
-        expect(
-          await controller.signUp(
-            {
-              email: 'testUser@icloud.com',
-              password: 'password',
-              fullname: 'subomi',
-              username: '545plea',
-            },
-            mockResponse,
-            '127.0.0.1',
-          ),
-        ).toEqual({ message: 'success' });
+    it('signUp - it should create the user', async () => {
+      myDatabaseServiceMock.users.create.mockResolvedValue(mockUser);
 
-        expect(myDatabaseServiceMock.refreshTokens.create).toHaveBeenCalled();
-        expect(mockRedisServiceMock.getFromCache).toHaveBeenCalled();
+      myDatabaseServiceMock.refreshTokens.create.mockResolvedValue(null);
+
+      mySecretsManagerServiceMock.getSecret.mockResolvedValue({
+        status: true,
+        data: { JWT_SECRET: '123456' },
+        error: null,
       });
+
+      expect(
+        await controller.signUp(
+          {
+            email: 'testUser@icloud.com',
+            password: 'password',
+            fullname: 'subomi',
+            username: '545plea',
+          },
+          mockResponse,
+          '127.0.0.1',
+        ),
+      ).toEqual({ message: 'success' });
+
+      expect(myDatabaseServiceMock.refreshTokens.create).toHaveBeenCalled();
+      expect(mySecretsManagerServiceMock.getSecret).toHaveBeenCalled();
+    });
+
+    // it('signIn - it should signIn the user', async () => {
+    //   myRedisServiceMock.getFromCache.mockResolvedValue({
+    //     status: true,
+    //     data: 0,
+    //     error: null,
+    //   });
+
+    //   myDatabaseServiceMock.users.findUnique.mockResolvedValue(mockUser);
+
+    //   myDatabaseServiceMock.refreshTokens.create.mockResolvedValue(null);
+
+    //   mySecretsManagerServiceMock.getSecret.mockResolvedValue({
+    //     status: true,
+    //     data: { JWT_SECRET: '123456' },
+    //     error: null,
+    //   });
+
+    //   expect(
+    //     await controller.signIn(
+    //       {
+    //         password: 'password',
+    //         username: '545plea',
+    //       },
+    //       mockResponse,
+    //       '127.0.0.1',
+    //     ),
+    //   ).toEqual({ message: 'success' });
+
+    //   expect(myDatabaseServiceMock.refreshTokens.create).toHaveBeenCalled();
+    //   expect(mySecretsManagerServiceMock.getSecret).toHaveBeenCalled();
+    // });
+  });
+
+  describe('Unsuccessfull Requests', () => {
+    it('it should not create the user due to conflicting records', async () => {
+      myDatabaseServiceMock.users.create
+        .mockRejectedValueOnce(
+          new PrismaClientKnownRequestError('Username is taken', {
+            code: 'P2002',
+            clientVersion: '7.0',
+          }),
+        )
+        .mockResolvedValue(mockUser);
+
+      myDatabaseServiceMock.refreshTokens.create.mockResolvedValue(null);
+
+      mySecretsManagerServiceMock.getSecret.mockResolvedValueOnce({
+        status: false,
+        data: null,
+        error: 'Failed to get secret',
+      });
+
+      await expect(
+        controller.signUp(
+          {
+            email: 'testUser@icloud.com',
+            password: 'password',
+            fullname: 'subomi',
+            username: '545plea',
+          },
+          mockResponse,
+          '127.0.0.1',
+        ),
+      ).rejects.toThrow(ConflictException);
+    });
+
+    it('it should fail because it failed to get secrets', async () => {
+      myDatabaseServiceMock.users.create.mockResolvedValue(mockUser);
+
+      myDatabaseServiceMock.refreshTokens.create.mockResolvedValue(null);
+
+      mySecretsManagerServiceMock.getSecret.mockResolvedValue({
+        status: false,
+        data: null,
+        error: 'Failed to get secret',
+      });
+
+      await expect(
+        controller.signUp(
+          {
+            email: 'testUser@icloud.com',
+            password: 'password',
+            fullname: 'subomi',
+            username: '545plea',
+          },
+          mockResponse,
+          '127.0.0.1',
+        ),
+      ).rejects.toThrow(InternalServerErrorException);
     });
   });
 });
