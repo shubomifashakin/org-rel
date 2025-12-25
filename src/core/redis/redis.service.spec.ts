992ff68d4d52f629aef1c8fd5afbdc03feaaa5eb
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-12-25 21:31:59 +0100

    updated redis service test

--- src/core/redis/redis.service.spec.ts
+++ src/core/redis/redis.service.spec.ts
@@ -1,5 +1,8 @@
+import { ClsModule } from 'nestjs-cls';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RedisService } from './redis.service.js';
+import { AppConfigModule } from '../app-config/app-config.module.js';
+import { AppLoggerModule } from '../app-logger/app-logger.module.js';
 
 describe('RedisService', () => {
   let service: RedisService;
@@ -7,6 +10,22 @@ describe('RedisService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [RedisService],
+      imports: [
+        AppConfigModule,
+        AppLoggerModule,
+        ClsModule.forRoot({
+          global: true,
+          middleware: {
+            mount: true,
+            generateId: true,
+            idGenerator: () => 'test-request-id',
+            setup: (clx) => {
+              clx.set('ip', '127.0.0.1');
+              clx.set('userAgent', 'test-agent');
+            },
+          },
+        }),
+      ],
     }).compile();
 
     service = module.get<RedisService>(RedisService);
